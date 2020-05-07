e5fe86202b9514076d282a36376c041f9d4b035e
Ramanpreet Kaur Kohli <51257414+KohliRamanpreet@users.noreply.github.com>
2020-05-07 13:11:19 +0530

    Update app.module.ts

--- image-searchUpload/src/app/app.module.ts
+++ image-searchUpload/src/app/app.module.ts
@@ -4,14 +4,9 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageListComponent } from './image-list/image-list.component';
-import {MatCardModule} from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-import { NgxMasonryModule } from 'ngx-masonry';
 import {ImageService} from '../app/image.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -21,13 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatCardModule,
-   BrowserAnimationsModule,
     HttpClientModule,
-    MatInputModule,
     FormsModule,
-    NgxMasonryModule
+    MatCardModule,
+    
   ],
   providers: [ImageService],
   bootstrap: [AppComponent]
